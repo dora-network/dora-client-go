10c8986b6d6dbeee738477d13a55711bc66c55d4
Tan Quach <tan@dora.tech>
2025-07-31 14:08:23 +0200

    fix: list assets response should be array in public api

--- doraclient/model_list_assets_response.go
+++ doraclient/model_list_assets_response.go
@@ -9,7 +9,7 @@
 package doraclient
 
 type ListAssetsResponse struct {
-	Data *Asset `json:"data,omitempty"`
+	Data []Asset `json:"data,omitempty"`
 	// The error message. Present for error (non-2xx) responses.
 	Error_ string `json:"error,omitempty"`
 	Metadata *Metadata `json:"metadata,omitempty"`
